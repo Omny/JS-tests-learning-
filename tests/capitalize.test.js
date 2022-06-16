30d07556355159c302a43090b223fd1a491d6a67
Artem Petrishchev <artem.omny@gmail.com>
2022-06-16 14:10:49 +0300

    Change to assert.deepStrictEqual

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,7 @@
 import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-assert.deepEqual(capitalize(''), '');
-assert.deepEqual(capitalize('hello'), 'Hello');
+assert.deepStrictEqual(capitalize(''), '');
+assert.deepStrictEqual(capitalize('hello'), 'Hello');
 
 console.log('All tests finished!');
